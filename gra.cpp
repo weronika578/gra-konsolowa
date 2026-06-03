32ded5dc02cba39ae28940dc4fc18bf0de9a2c9c
weronika578 <weronika.wolschlaeger@gmail.com>
2026-06-03 12:46:03 +0200

    część planszy

---
+++ gra.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+char plansza [10][10];
+
+ for (int i=0;i<10;i++) {
+    for (int j=0;j<10;j++) {
+
+
+        if (i==0 || i==9 || j==0 || j==9)
+            {
+                plansza[i][j] = '#';
+            }
+        else
+            {
+            plansza[i][j] = ' ';
+            }
+cout << plansza[i][j] << endl;
+}
+}
+ }
+
