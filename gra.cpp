abe04ada276eb1993c9e38efd4a4e78828c64488
weronika578 <wolschlaeger.weronika@gmail.com>
2026-06-10 12:50:51 +0200

    cos

--- gra.cpp
+++ gra.cpp
@@ -10,15 +10,50 @@ char plansza [10][10];
 
         if (i==0 || i==9 || j==0 || j==9)
             {
-                plansza[i][j] = '#';
+                plansza[i][j] = '#' ;
+
             }
         else
             {
-            plansza[i][j] = ' ';
+            plansza[i][j] = '.';
             }
-cout << plansza[i][j];
 }
 }
- }
 
- 
+int z,p;
+int zx = 1;
+int zy = 1;
+int px = 8;
+int py = 8;
+
+plansza[2][4] = '$';
+plansza[4][6] = '$';
+plansza[6][2] = '$';
+plansza[8][4] = '$';
+
+plansza[zx][zy] = 'Z';
+plansza[px][py] = 'P';
+
+int ruchy = 20;
+int pkt = 0;
+
+while (ruchy > 0) {
+    system("cls");
+    cout << "Pozostale ruchy: " << ruchy  << " | Punkty: " << pkt  << "  / 4" << endl;
+
+
+
+
+for  (int i =0; i<10; i++) {
+    for (int j=0; j< 10;j++) {
+        cout << plansza[i][j] << " "; }
+        cout << endl; }}
+      //wyswietlanie planszy
+
+
+
+
+
+
+
+}
