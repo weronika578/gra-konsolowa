3baad0f37df94f666ad05ce2271bb4ae4c4f9139
weronika578 <wolschlaeger.weronika@gmail.com>
2026-06-11 13:33:13 +0200

    ostatnia infa

--- gra.cpp
+++ gra.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+ #include <iostream>
+ #include <conio.h>
 using namespace std;
 int main() {
 
@@ -20,7 +21,7 @@ char plansza [10][10];
 }
 }
 
-int z,p;
+
 int zx = 1;
 int zy = 1;
 int px = 8;
@@ -39,7 +40,7 @@ int pkt = 0;
 
 while (ruchy > 0) {
     system("cls");
-    cout << "Pozostale ruchy: " << ruchy  << " | Punkty: " << pkt  << "  / 4" << endl;
+    cout << "Pozostale ruchy: " << ruchy  << " | Punkty: " << pkt  << " / 4" << endl;
 
 
 
@@ -47,10 +48,37 @@ while (ruchy > 0) {
 for  (int i =0; i<10; i++) {
     for (int j=0; j< 10;j++) {
         cout << plansza[i][j] << " "; }
-        cout << endl; }}
+        cout << endl; }
       //wyswietlanie planszy
 
+char ruch;
+cout << "Ruch W,A,S,D: ";
+ruch = getch();
+plansza[zx][zy] = '.';
 
+if ((ruch=='W' || ruch=='w') && plansza[zx-1][zy] != '#') zx--;
+if ((ruch=='A' || ruch=='a') && plansza[zx+1][zy] != '#') zy--;
+if ((ruch=='S' || ruch=='s') && plansza[zx+1][zy] != '#') zx++;
+if ((ruch=='D' || ruch=='d') && plansza[zx][zy+1] != '#') zy++;
+if (plansza[zx][zy] == '$') { pkt++;}
+plansza[zx][zy] = 'Z';
+plansza[px][py] = '.';
+
+if (px < zx) px++;
+else if (px > zx) px--;
+
+if (py < zy) py++;
+else if (py > zy) py--;
+plansza[px][py] = 'P';
+
+if (px == zx && py == zy) {
+    system("cls");
+    cout << "PRZEGRANA! Policjant cie zlapal!" << endl;
+    break;
+}
+ruchy--;
+
+}
 
 
 
