5be97d197b0e99170de939b07405c847389d036e
weronika578 <wolschlaeger.weronika@gmail.com>
2026-06-13 20:27:33 +0100

    Koniec projektu

--- gra.cpp
+++ gra.cpp
@@ -26,6 +26,7 @@ int zx = 1;
 int zy = 1;
 int px = 8;
 int py = 8;
+char pp = '.';
 
 plansza[2][4] = '$';
 plansza[4][6] = '$';
@@ -40,6 +41,10 @@ int pkt = 0;
 
 while (ruchy > 0) {
     system("cls");
+    cout << "Weronika Wolschlaeger 1a" << endl;
+
+cout << "Cel gry: Zdobadz wszystkie skarby w 20 ruchach i nie daj sie zlapac przez policjanta" << endl;
+ cout << endl;
     cout << "Pozostale ruchy: " << ruchy  << " | Punkty: " << pkt  << " / 4" << endl;
 
 
@@ -57,18 +62,19 @@ ruch = getch();
 plansza[zx][zy] = '.';
 
 if ((ruch=='W' || ruch=='w') && plansza[zx-1][zy] != '#') zx--;
-if ((ruch=='A' || ruch=='a') && plansza[zx+1][zy] != '#') zy--;
+if ((ruch=='A' || ruch=='a') && plansza[zx][zy-1] != '#') zy--;
 if ((ruch=='S' || ruch=='s') && plansza[zx+1][zy] != '#') zx++;
 if ((ruch=='D' || ruch=='d') && plansza[zx][zy+1] != '#') zy++;
 if (plansza[zx][zy] == '$') { pkt++;}
 plansza[zx][zy] = 'Z';
-plansza[px][py] = '.';
+plansza[px][py] = pp;
 
 if (px < zx) px++;
 else if (px > zx) px--;
 
-if (py < zy) py++;
+else if (py < zy) py++;
 else if (py > zy) py--;
+pp = plansza[px][py];
 plansza[px][py] = 'P';
 
 if (px == zx && py == zy) {
@@ -76,8 +82,26 @@ if (px == zx && py == zy) {
     cout << "PRZEGRANA! Policjant cie zlapal!" << endl;
     break;
 }
+if (pkt ==4) {
+    system("cls");
+    cout << "WYGRANA!" << endl;
+    break;
+}
 ruchy--;
 
+
+  if (pkt == 4) {
+      system("cls");
+      cout << "WYGRANA! Zebralas wszystkie skarby!" << endl;
+      break;
+ }
+
+  if (ruchy <= 0) {
+      system("cls");
+     cout << "Pozostale ruchy: 0 | Punkty: " << pkt << " / 4" << endl << endl;
+      cout << "PRZEGRANA! Wykorzystalas maksymalna ilosc ruchow." << endl;
+      break;
+  }
 }
 
 
